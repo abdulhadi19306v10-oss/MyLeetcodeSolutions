a335d9f997b594b2955413437b5f84ec5baf0687
Abdul Hadi <abdulhadi19306v1.0@gmail.com>
2026-06-30 23:14:33 +0500

    Time: 0 ms (100%), Space: 95.2 MB (61.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    // We always want to run binary search on the smaller array for efficiency.
     if (nums1.size() > nums2.size()) {
         return findMedianSortedArrays(nums2, nums1);
     }
@@ -19,14 +18,12 @@ public:
         int partitionX = (low + high) / 2;
         int partitionY = (x + y + 1) / 2 - partitionX;
 
-        // If partition is 0, there is nothing on the left side. Use -infinity.
         double maxLeftX = (partitionX == 0) ? -1e9 : nums1[partitionX - 1];
         double minRightX = (partitionX == x) ? 1e9 : nums1[partitionX];
 
         double maxLeftY = (partitionY == 0) ? -1e9 : nums2[partitionY - 1];
         double minRightY = (partitionY == y) ? 1e9 : nums2[partitionY];
 
-        // Check if we found the perfect split
         if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
             if ((x + y) % 2 == 0) {
                 return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
@@ -34,15 +31,13 @@ public:
                 return max(maxLeftX, maxLeftY);
             }
         } 
-        // We are too far on the right side of nums1. Move left.
         else if (maxLeftX > minRightY) {
             high = partitionX - 1;
         } 
-        // We are too far on the left side of nums1. Move right.
         else {
             low = partitionX + 1;
         }
     }
-    return 0.0; // Fallback
+    return 0.0;
     }
 };
